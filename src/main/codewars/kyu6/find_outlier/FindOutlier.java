af4d0e798d486d1dd9ce0a9e081224ae38dbabd2
Kvitto <kian04@mail.ru>
2024-11-21 17:00:45 +0300

    Kyu6_FindOutlier_fix
    - Fix % for negative integers

--- src/main/codewars/kyu6/find_outlier/FindOutlier.java
+++ src/main/codewars/kyu6/find_outlier/FindOutlier.java
@@ -2,29 +2,14 @@ package codewars.kyu6.find_outlier;
 
 public class FindOutlier {
     static int find(int[] integers) {
-        int even = -1;
-        for (int i = 0; i < integers.length; ){
-            int a = integers[i] % 2;
-            int b = integers[i + 1] % 2;
-            int c = integers[i + 2] % 2;
-            if (a == b && a == c) {
-                if (even == -1) {even = integers[i] % 2;}
-                i = i + 6 < integers.length ? i + 3 : i + 1;
-            } else if (a == b) {
-                if (even != -1 && integers[i + 2] % 2 == even) {
-                    return integers[i];
-                }
-                return integers[i + 2];
-            } else if (b == c) {
-                if (even != -1 && integers[i] % 2 == even) {
-                    return integers[i + 1];
-                }
-                return integers[i];
-            } else {
-                if (even != -1 && integers[i + 1] % 2 == even) {
-                    return integers[i];
-                }
-                return integers[i + 1];
+        int odd = -1;
+        for(int i = 0; i < 3; i++) {
+            if(Math.abs(integers[i]) % 2 == 1) {odd++;}
+        }
+        odd = odd > 0 ? 1 : 0;
+        for (int integer : integers) {
+            if (Math.abs(integer) % 2 != odd) {
+                return integer;
             }
         }
         return 0;
--- src/test/codewars/kyu6/find_outlier/FindOutlierTest.java
+++ src/test/codewars/kyu6/find_outlier/FindOutlierTest.java
@@ -9,6 +9,7 @@ public class FindOutlierTest {
     @Test
     public void find() {
         assertEquals(3, FindOutlier.find(new int[] {2, 6, 8, 10, 3, 3, 2, 6, 8, 200, 700, 1, 84, 10, 4, 1, 17, 6, 8, 10, 6, 12, 24, 36, 36}));
+        assertEquals(-6, FindOutlier.find(new int[] {-5, -6, -7, -3, -11, -3, -9}));
         assertEquals(206847684, FindOutlier.find(new int[] {206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781}));
         assertEquals(0, FindOutlier.find(new int[] {Integer.MAX_VALUE, 0, 1}));
     }
