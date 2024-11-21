394ba41a1ea54bd5c180506a362b5829b096acaa
Kvitto <kian04@mail.ru>
2024-11-22 00:02:40 +0300

    Kyu6_toCamelCase_Best_Practice

--- src/main/codewars/kyu6/to_camel_case/Solution.java
+++ src/main/codewars/kyu6/to_camel_case/Solution.java
@@ -1,16 +1,10 @@
 package codewars.kyu6.to_camel_case;
 
-import java.lang.StringBuilder;
+import java.util.regex.Pattern;
 
-public class Solution{
+class Solution{
 
     static String toCamelCase(String s){
-        String[] strings = s.split("[-\\_]");
-        StringBuilder camelCase = new StringBuilder();
-        camelCase.append(strings[0]);
-        for (int i = 1; i < strings.length; i++) {
-            camelCase.append(strings[i].substring(0, 1).toUpperCase()).append(strings[i].substring(1).toLowerCase());
-        }
-        return String.valueOf(camelCase);
+        return Pattern.compile("[-|_](.)").matcher(s).replaceAll(r -> r.group(1).toUpperCase());
     }
 }
