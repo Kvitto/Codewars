122430760df0af34909615035b91c4826015c269
Kvitto <kian04@mail.ru>
2024-11-22 17:16:08 +0300

    Kyu6_Persist_best_practice

--- src/main/codewars/kyu6/persistence/Persist.java
+++ src/main/codewars/kyu6/persistence/Persist.java
@@ -1,12 +1,10 @@
 package codewars.kyu6.persistence;
 
-import java.util.Arrays;
-
 class Persist {
     public static int persistence(long n) {
         int count = 0;
         while (String.valueOf(n).length() > 1) {
-            n = Arrays.stream(String.valueOf(n).split("")).mapToInt(Integer::parseInt).reduce(1, (a,b) -> a*b);
+            n = Long.toString(n).chars().reduce(1, (a,b) -> a * (b - '0'));
             count++;
         }
         return count;
