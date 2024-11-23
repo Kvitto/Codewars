691236695f20ede623173bad36ec35ee7b07f8f7
Kvitto <kian04@mail.ru>
2024-11-23 12:40:41 +0300

    Kyu6_Order_best_practice

--- src/main/codewars/kyu6/order/Order.java
+++ src/main/codewars/kyu6/order/Order.java
@@ -1,13 +1,16 @@
 package codewars.kyu6.order;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class Order {
     public static String order(String words) {
         if (words.isEmpty()) return "";
-        return String.join(" ", Arrays.stream(words.split(" "))
-                .collect(Collectors.toMap(s -> Integer.parseInt(s.replaceAll("[^0-9]", "")), s -> s))
-                .values());
+//        return String.join(" ", Arrays.stream(words.split(" "))
+//                .collect(Collectors.toMap(s -> Integer.parseInt(s.replaceAll("[^0-9]", "")), s -> s))
+//                .values());
+
+        return Arrays.stream(words.split(" ")).sorted(Comparator.comparing(s -> Integer.parseInt(s.replaceAll("[\\D]", "")))).collect(Collectors.joining(" "));
     }
 }
