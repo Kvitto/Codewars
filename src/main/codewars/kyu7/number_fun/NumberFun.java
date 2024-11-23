71f7ab8753d28235b1bf55850b76c74386874d8f
Kvitto <kian04@mail.ru>
2024-11-23 20:58:37 +0300

    Kyu7_NumberFun

---
+++ src/main/codewars/kyu7/number_fun/NumberFun.java
@@ -0,0 +1,7 @@
+package codewars.kyu7.number_fun;
+
+public class NumberFun {
+    public static long findNextSquare(long sq) {
+        return (Math.sqrt(sq) % 1) == 0 ? (long) Math.pow(Math.sqrt(sq) + 1, 2) : -1;
+    }
+}
