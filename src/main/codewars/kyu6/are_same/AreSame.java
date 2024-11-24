67bde28c428f146e6684dca5fd6182dc584bfa1f
Kvitto <kian04@mail.ru>
2024-11-24 21:48:16 +0300

    Kyu6_AreSame_best_practice

--- src/main/codewars/kyu6/are_same/AreSame.java
+++ src/main/codewars/kyu6/are_same/AreSame.java
@@ -4,12 +4,6 @@ import java.util.Arrays;
 
 public class AreSame {
     public static boolean comp(int[] a, int[] b) {
-        if (a == null || b == null || a.length != b.length) return false;
-        a = Arrays.stream(a).map(Math::abs).sorted().toArray();
-        Arrays.sort(b);
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != Math.sqrt(b[i])) return false;
-        }
-        return true;
+        return a != null && b != null && a.length == b.length && Arrays.equals(Arrays.stream(a).map(i -> i * i).sorted().toArray(), Arrays.stream(b).sorted().toArray());
     }
 }
