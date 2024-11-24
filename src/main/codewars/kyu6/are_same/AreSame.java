84c968c86927eac548420a4ceb71e8b8788eacd3
Kvitto <kian04@mail.ru>
2024-11-24 21:47:10 +0300

    Kyu6_AreSame_my_variant

--- src/main/codewars/kyu6/are_same/AreSame.java
+++ src/main/codewars/kyu6/are_same/AreSame.java
@@ -1,7 +1,15 @@
 package codewars.kyu6.are_same;
 
+import java.util.Arrays;
+
 public class AreSame {
     public static boolean comp(int[] a, int[] b) {
+        if (a == null || b == null || a.length != b.length) return false;
+        a = Arrays.stream(a).map(Math::abs).sorted().toArray();
+        Arrays.sort(b);
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != Math.sqrt(b[i])) return false;
+        }
         return true;
     }
 }
