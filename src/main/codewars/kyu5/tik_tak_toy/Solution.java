e922ad9e96d5b8f34e7cf9019f308977b378dd80
Kvitto <kian04@mail.ru>
2024-11-26 02:03:10 +0300

    Kyu5_TikTakToy_best_practice

--- src/main/codewars/kyu5/tik_tak_toy/Solution.java
+++ src/main/codewars/kyu5/tik_tak_toy/Solution.java
@@ -1,53 +1,27 @@
 package codewars.kyu5.tik_tak_toy;
 
 public class Solution {
-    static final int FIRST_WIN_SUM = 3;
-    static final int SECOND_WIN_SUM = 6;
-    static boolean isFree;
-    static int status;
 
     public static int isSolved(int[][] board) {
-        isFree = false;
-        status = -2;
-        for (int i = 0; i < board.length; i++) {
-            if (scan(getLine(board, i, i + 1, 0, board[0].length, false)) > 0) return status;
-        }
-        for (int i = 0; i < board.length; i++) {
-            if (scan(getLine(board, 0, board.length, i, i + 1, false)) > 0) return status;
-        }
-        if (scan(getLine(board, 0, board.length, 0, board.length, true)) > 0) return status;
-        if (scan(getLine(board, 0, board.length, board.length, 0, true)) > 0) return status;
-        return isFree ? -1 : 0;
-    }
-
-    public static int scan(int[] line) {
-        int sum = 0;
-        for (int i : line) {
-            if (i == 0) {
-                isFree = true;
-                return 0;
-            }
-            sum += i;
-        }
-        if (sum == FIRST_WIN_SUM) return status = 1;
-        if (sum == SECOND_WIN_SUM) return status = 2;
-        return 0;
-    }
-
-    public static int[] getLine(int[][] board, int xFrom, int xTo, int yFrom, int yTo, boolean diag) {
-        int[] result = new int[board.length];
-        int k = 0;
-        for (int i = xFrom; i < xTo; i++) {
-            if (!diag) {
-                for (int j = yFrom; j < yTo; j++) {
-                    result[k++] = board[i][j];
+        int[][] x = new int[2][8];
+        int r = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {
+                    r = -1;
+                    continue;
                 }
-            } else if (yTo - yFrom > 0) {
-                result[k++] = board[i][i];
-            } else {
-                result[k++] = board[i][board.length - i - 1];
+                int a = board[i][j] % 2;
+                x[a][i]++;
+                x[a][j + 3]++;
+                if (i == j) x[a][6]++;
+                if (i == 2 - j) x[a][7]++;
             }
         }
-        return result;
+        for (int i = 0; i < 8; i++) {
+            if (x[0][i] == 3) return 2;
+            if (x[1][i] == 3) return 1;
+        }
+        return r;
     }
 }
