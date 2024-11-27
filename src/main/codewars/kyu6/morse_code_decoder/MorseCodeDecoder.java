dcca28398f2ac7d9043d1281f91a26ddcee51357
Kvitto <kian04@mail.ru>
2024-11-27 19:45:10 +0300

    Kyu6_MorseCodeDecoder

---
+++ src/main/codewars/kyu6/morse_code_decoder/MorseCodeDecoder.java
@@ -0,0 +1,8 @@
+package codewars.kyu6.morse_code_decoder;
+
+public class MorseCodeDecoder {
+    public static String decode(String morseCode) {
+        // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
+    return "morseCode";
+    }
+}
---
+++ src/test/codewars/kyu6/morse_code_decoder/MorseCodeDecoderTest.java
@@ -0,0 +1,12 @@
+package codewars.kyu6.morse_code_decoder;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class MorseCodeDecoderTest {
+    @Test
+    public void testExampleFromDescription() {
+        assertThat(MorseCodeDecoder.decode(".... . -.--   .--- ..- -.. ."), is("HEY JUDE"));
+    }
+}
