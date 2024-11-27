4e10f4425a043328608561ca89942b601c77b139
Kvitto <kian04@mail.ru>
2024-11-27 21:11:21 +0300

    Kyu6_MorseCodeDecoder_my_variant

--- src/main/codewars/kyu6/morse_code_decoder/MorseCodeDecoder.java
+++ src/main/codewars/kyu6/morse_code_decoder/MorseCodeDecoder.java
@@ -1,8 +1,14 @@
 package codewars.kyu6.morse_code_decoder;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class MorseCodeDecoder {
     public static String decode(String morseCode) {
-        // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
-    return "morseCode";
+        return Arrays.stream(morseCode.trim().split("   ")).map(MorseCodeDecoder::decoding).collect(Collectors.joining(" "));
+    }
+
+    static String decoding(String morseCode) {
+        return Arrays.stream(morseCode.split(" ")).map(MorseCode::get).collect(Collectors.joining());
     }
 }
