4ab88025aafdf09ea6a0d13b0ccea505a23a9714
Kvitto <kian04@mail.ru>
2024-11-30 13:29:30 +0300

    Kyu4_Permutations_blank

---
+++ src/main/codewars/kyu4/permutations/Permutations.java
@@ -0,0 +1,11 @@
+package codewars.kyu4.permutations;
+
+import java.util.List;
+
+class Permutations {
+
+    public static List<String> singlePermutations(String s) {
+        // Your code here!
+        return null;
+    }
+}
