a0e90b23e3da9a4fbf817ed55e646036529fa422
Kvitto <kian04@mail.ru>
2024-12-09 18:37:21 +0300

    Kyu4_Permutations_Best

--- src/main/codewars/kyu4/permutations/Permutations.java
+++ src/main/codewars/kyu4/permutations/Permutations.java
@@ -5,28 +5,44 @@ import java.util.*;
 class Permutations {
 
     public static List<String> singlePermutations(String s) {
-        System.out.println(s);
-        if (s.isBlank()) return null;
-        Set<String> res = new HashSet<>(shuffle(s));
-        return new ArrayList<>(res);
-    }
-
-    private static List<String> shuffle(String str) {
-        int length = str.length();
-        if (length <= 1) return List.of(str.split(""));
-        if (length == 2) {
-            String substring = str.substring(length - 2);
-            return List.of(substring, new StringBuilder(substring).reverse().toString());
-        }
-        List<String> res = new ArrayList<>();
-        String[] split = str.split("");
-        for (int i = 0; i < split.length; i++) {
-            String a = split[i];
-            String sub = str.substring(0, i) + str.substring(i + 1);
-            for (String b : shuffle(sub)) {
-                res.add(a + b);
+        Set<String> set = new HashSet<>();
+        if (s.length() == 1) {
+            set.add(s);
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                List<String> temp = singlePermutations(s.substring(0, i) + s.substring(i + 1));
+                for (String string : temp) {
+                    set.add(s.charAt(i) + string);
+                }
             }
         }
-        return res;
+        return new ArrayList<>(set);
     }
+
+
+//    public static List<String> singlePermutations(String s) {
+//        System.out.println(s);
+//        if (s.isBlank()) return null;
+//        Set<String> res = new HashSet<>(shuffle(s));
+//        return new ArrayList<>(res);
+//    }
+//
+//    private static List<String> shuffle(String str) {
+//        int length = str.length();
+//        if (length <= 1) return List.of(str.split(""));
+//        if (length == 2) {
+//            String substring = str.substring(length - 2);
+//            return List.of(substring, new StringBuilder(substring).reverse().toString());
+//        }
+//        List<String> res = new ArrayList<>();
+//        String[] split = str.split("");
+//        for (int i = 0; i < split.length; i++) {
+//            String a = split[i];
+//            String sub = str.substring(0, i) + str.substring(i + 1);
+//            for (String b : shuffle(sub)) {
+//                res.add(a + b);
+//            }
+//        }
+//        return res;
+//    }
 }
