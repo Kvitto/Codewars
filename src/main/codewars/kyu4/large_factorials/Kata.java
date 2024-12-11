3e34cb04414c7c1747783ef515f8c25a1eb21cb9
Kvitto <kian04@mail.ru>
2024-12-11 23:23:09 +0300

    Kyu4_Large_Factorials_best_practice

--- src/main/codewars/kyu4/large_factorials/Kata.java
+++ src/main/codewars/kyu4/large_factorials/Kata.java
@@ -3,13 +3,15 @@ package codewars.kyu4.large_factorials;
 import java.math.BigInteger;
 
 public class Kata {
-    public static String Factorial(int i) {
-        return String.valueOf(calcFac(BigInteger.valueOf(i)));
-    }
+    public static String Factorial(int n) {
+        return java.util.stream.IntStream.rangeClosed(1, n)
+                .mapToObj(BigInteger::valueOf)
+                .reduce(BigInteger.ONE, BigInteger::multiply)
+                .toString();
 
-    private static BigInteger calcFac(BigInteger i) {
-        if (i.equals(BigInteger.ONE)) return BigInteger.ONE;
-        return i.multiply(calcFac(i.subtract(BigInteger.ONE)));
-    }
+//        return n < 2
+//                ? "1"
+//                : BigInteger.valueOf(n).multiply(new BigInteger(Factorial(n-1))).toString();
 
+    }
 }
