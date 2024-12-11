7d2fdbb3ef6b8fd7df1890146bef2bf917bac1e8
Kvitto <kian04@mail.ru>
2024-12-11 14:08:03 +0300

    Kyu3_Spiralizor_my_variant

--- src/main/codewars/kyu3/spiral/Spiralizor.java
+++ src/main/codewars/kyu3/spiral/Spiralizor.java
@@ -1,7 +1,36 @@
 package codewars.kyu3.spiral;
 
+import java.util.Arrays;
+
 public class Spiralizor {
+    static int[][] qube;
+
     public static int[][] spiralize(int size) {
-        return null;
+        fillZero(size);
+        turnTheSpiral(0, size - 1);
+        return qube;
     }
+
+    private static void turnTheSpiral(int a, int b) {
+        for (int i = a; i <= b; i++) {
+            qube[a][i] = 1;
+            qube[i][b] = 1;
+            qube[b][a + b - i] = 1;
+            qube[a + b - i][a] = 1;
+        }
+        a += 2;
+        b -= 2;
+        qube[a - 1][a - 2] = 0;
+        if (b < a) return;
+        qube[a][a - 1] = 1;
+        turnTheSpiral(a, b);
+    }
+
+    public static void fillZero(int size) {
+        qube = new int[size][size];
+        for (int[] x : qube){
+            Arrays.fill(x, 0);
+        }
+    }
+
 }
