85391062f8b08787aca6a13fd7a3390a65f24e59
Kvitto <kian04@mail.ru>
2024-12-12 21:51:28 +0300

    Kyu4_Rectangle_Rotation_blank

---
+++ src/main/codewars/kyu4/rectangle_rotation/Solution.java
@@ -0,0 +1,7 @@
+package codewars.kyu4.rectangle_rotation;
+
+public class Solution {
+    static int rectangleRotation(final int a, final int b) {
+        return 0;
+    }
+}
---
+++ src/test/codewars/kyu4/rectangle_rotation/SolutionTest.java
@@ -0,0 +1,14 @@
+package codewars.kyu4.rectangle_rotation;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void BasicTests() {
+        assertEquals(23, Solution.rectangleRotation(6,4));
+        assertEquals(65, Solution.rectangleRotation(30,2));
+        assertEquals(49, Solution.rectangleRotation(8,6));
+        assertEquals(333, Solution.rectangleRotation(16,20));
+    }
+}
